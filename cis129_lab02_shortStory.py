df9818f461315e41b1574ec2bdff19907d30877a
stellarmood <jfeddick@mail.pima.edu>
2025-02-18 15:41:38 -0700

    Update cis129_lab02_shortStory.py
    story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello world!')
+print('Once upon a time there was a house on a hill, many stories were told about this house. The house was had a name but in the songs sang about the house none mentioned it. There was a kid no one noticed he had a stark interest in this house, he wanted to know the name of this house. So he set out to find it and bare witness to it.')
+print('He knew where it was but he didnt have means to get there')
+print('So he asked his mother and she said no and thats when he decided he was going to skip school and bike there, and that he did.')
+#author;james feddick 
+#github project 1 
+#prints a short story about a boy who wants to go to a house. Deleted hello world.
