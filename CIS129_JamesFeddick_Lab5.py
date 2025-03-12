45e466619d25b45cbe5d2e87acf450d3582e2139
stellarmood <jfeddick@mail.pima.edu>
2025-03-12 11:09:24 -0700

    Add files via upload

---
+++ CIS129_JamesFeddick_Lab5.py
@@ -0,0 +1,14 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:\Users\tream\AppData\Local\Programs\Python\Python312\lab 5.py ===
+Enter number of bottles for day #1: 345
+Enter number of bottles for day #2: 238
+Enter number of bottles for day #3: 638
+Enter number of bottles for day #4: 890
+Enter number of bottles for day #5: 1035
+Enter number of bottles for day #6: 899
+Enter number of bottles for day #7: 536
+The total number of bottles collected is 4581
+The total paid out is $ 458.1
+Do you want to enter another week’s worth of data? (Enter y or n): n
